a4863ec512e11e5e5fdcab0824f755681c3b1b2d
Faraphel <rc60650@hotmail.com>
2023-12-24 11:00:58 +0100

    language should now be detected automatically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt6.QtCore import QTranslator
+from PyQt6.QtCore import QTranslator, QLocale
 from PyQt6.QtWidgets import QApplication
 
 from source import assets_path
@@ -11,10 +11,14 @@ if __name__ == "__main__":
     # create the application
     application = QApplication(sys.argv)
 
+    # get the user language
+    local = QLocale()
+    language_code: str = local.languageToCode(local.language())
+
     # load the translator to support multiple languages
     translator = QTranslator()
     application.installTranslator(translator)
-    translator.load(str(assets_path / "language/french.qm"))
+    translator.load(str(assets_path / f"language/{language_code}.qm"))
 
     # create the window
     window = MyMainWindow()
