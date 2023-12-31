f7af75b0b869b0043c2eb8db11cace398265329c
Faraphel <rc60650@hotmail.com>
2023-12-31 12:06:09 +0100

    added menu translations

--- assets/language/en.ts
+++ assets/language/en.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="fr_FR">
+<TS version="2.1" language="en">
 <context>
     <name>SurveyEngine</name>
     <message>
         <location filename="../../source/widget/SurveyEngine.py" line="177"/>
         <source>WARNING</source>
-        <translation>Avertissement</translation>
+        <translation>Warning</translation>
     </message>
 </context>
 <context>
@@ -14,17 +14,17 @@
     <message>
         <location filename="../../source/widget/SurveyNavigation.py" line="19"/>
         <source>ABANDON</source>
-        <translation>Abandonner</translation>
+        <translation>Abandon</translation>
     </message>
     <message>
         <location filename="../../source/widget/SurveyNavigation.py" line="29"/>
         <source>SKIP</source>
-        <translation>Passer</translation>
+        <translation>Skip</translation>
     </message>
     <message>
         <location filename="../../source/widget/SurveyNavigation.py" line="38"/>
         <source>NEXT</source>
-        <translation>Suivant</translation>
+        <translation>Next</translation>
     </message>
 </context>
 <context>
@@ -32,7 +32,7 @@
     <message>
         <location filename="../../source/widget/SurveyWindow.py" line="16"/>
         <source>SURVEY</source>
-        <translation>Sondage</translation>
+        <translation>Survey</translation>
     </message>
 </context>
 </TS>
